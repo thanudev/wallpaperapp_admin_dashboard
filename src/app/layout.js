40e5eb567cd4464bcf8edad7e2fbcad70f21bd38
thanudev <thanudevelopments@gmail.com>
2024-04-21 19:56:30 +0530

    Name Changed ..

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ["outfit"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Wallpaper app Admin DashBoard",
